9555713afc9a8f231c405bc7f906c51514fbb0c4
aayush <aayushkothari11@yahoo.com>
2018-10-19 13:57:58 +0530

    updated db

---
+++ Database/190ct_1:57.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 19, 2018 at 01:56 PM
+-- Server version: 5.7.23-0ubuntu0.16.04.1
+-- PHP Version: 7.0.32-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `college`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alumni`
+--
+
+CREATE TABLE `alumni` (
+  `sapid` bigint(11) NOT NULL,
+  `year_of_passing` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `committee`
+--
+
+CREATE TABLE `committee` (
+  `name` varchar(30) NOT NULL,
+  `incharge` bigint(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `number` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `current`
+--
+
+CREATE TABLE `current` (
+  `sapid` bigint(11) NOT NULL,
+  `fees` float NOT NULL,
+  `division` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `committee` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `date` date NOT NULL,
+  `time` time(6) DEFAULT '00:00:00.000000',
+  `Location` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `sapid` bigint(11) NOT NULL,
+  `salary` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person`
+--
+
+CREATE TABLE `person` (
+  `sapid` bigint(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `dob` date NOT NULL,
+  `mobile` int(10) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `dept` varchar(20) NOT NULL,
+  `age` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seminarhall`
+--
+
+CREATE TABLE `seminarhall` (
+  `Location` varchar(50) NOT NULL,
+  `opening_time` time(6) NOT NULL DEFAULT '00:00:00.000000',
+  `closing_time` time(6) NOT NULL DEFAULT '00:00:00.000000'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `sapid` bigint(11) NOT NULL,
+  `year_of_joining` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alumni`
+--
+ALTER TABLE `alumni`
+  ADD KEY `sapid` (`sapid`);
+
+--
+-- Indexes for table `committee`
+--
+ALTER TABLE `committee`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `incharge` (`incharge`);
+
+--
+-- Indexes for table `current`
+--
+ALTER TABLE `current`
+  ADD KEY `sapid` (`sapid`);
+
+--
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`name`),
+  ADD KEY `Location` (`Location`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD KEY `sapid` (`sapid`);
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`sapid`);
+
+--
+-- Indexes for table `seminarhall`
+--
+ALTER TABLE `seminarhall`
+  ADD PRIMARY KEY (`Location`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD KEY `sapid` (`sapid`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `alumni`
+--
+ALTER TABLE `alumni`
+  ADD CONSTRAINT `alumni_ibfk_1` FOREIGN KEY (`sapid`) REFERENCES `student` (`sapid`);
+
+--
+-- Constraints for table `committee`
+--
+ALTER TABLE `committee`
+  ADD CONSTRAINT `committee_ibfk_1` FOREIGN KEY (`incharge`) REFERENCES `faculty` (`sapid`);
+
+--
+-- Constraints for table `current`
+--
+ALTER TABLE `current`
+  ADD CONSTRAINT `current_ibfk_1` FOREIGN KEY (`sapid`) REFERENCES `student` (`sapid`);
+
+--
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`Location`) REFERENCES `seminarhall` (`Location`);
+
+--
+-- Constraints for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD CONSTRAINT `faculty_ibfk_1` FOREIGN KEY (`sapid`) REFERENCES `person` (`sapid`);
+
+--
+-- Constraints for table `student`
+--
+ALTER TABLE `student`
+  ADD CONSTRAINT `student_ibfk_1` FOREIGN KEY (`sapid`) REFERENCES `person` (`sapid`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
